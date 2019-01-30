c980450aeba09fa12a7a878fc2129a080dcf4e17
David Lavigueur <davidlavigueur@live.ca>
2019-01-30 11:05:19 -0500

    Ajout main()

--- tp1QA/src/tp1QA/CV.java
+++ tp1QA/src/tp1QA/CV.java
@@ -1,26 +1,37 @@
 package tp1QA;
+
 import java.io.*;
+
 public class CV {
-	
-	CV employeUn = new CV("Aoun","Karl","DES (Diplôme d'étude secondaire)",2,
-					"4 session technique informatique, technicien de point de vente 3 ans","utilisation de Git");
-	CV employeDeux = new CV("David","Lavigueur","DES (Diplôme d'étude secondaire)",2,"4 session technique informatique","Cours interactif et dynamique");
+
 	public static void main(String[] args) {
 		
+		CV employeUn = new CV("Aoun", "Karl", "DES (Diplôme d'étude secondaire)", 2,
+				"4 session technique informatique, technicien de point de vente 3 ans", "utilisation de Git");
 		
+		CV employeDeux = new CV("David", "Lavigueur", "DES (Diplôme d'étude secondaire)", 2,
+				"4 session technique informatique", "Cours interactif et dynamique");
+		employeUn.affiche();
+		employeDeux.affiche();
+
 	}
+
 	public CV() {
-		
+
 	}
-	public CV(String pnom, String pprenom, String pformation, int pnbAnneesExp, String pcompetence, String pattentes ) {
-		
+
+	public CV(String pnom, String pprenom, String pformation, int pnbAnneesExp, String pcompetence, String pattentes) {
+
 		String nom = pnom;
 		String prenom = pprenom;
 		String formation = pformation;
 		int nbAnneesExp = pnbAnneesExp;
 		String competence = pcompetence;
 		String attentes = pattentes;
+
 	}
-	
-	
+
+	public void affiche() {
+
+	}
 }

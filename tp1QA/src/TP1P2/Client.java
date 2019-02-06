82fea0393c6100c62e6160d18e4ffd300c498470
David Lavigueur <davidlavigueur@live.ca>
2019-02-06 12:00:49 -0500

    Ajout méthode calculerPrix

--- tp1QA/src/TP1P2/Client.java
+++ tp1QA/src/TP1P2/Client.java
@@ -3,20 +3,36 @@ package TP1P2;
 import java.util.ArrayList;
 
 public class Client {
-	String nom;
-	ArrayList<Produit> listeProduit = new ArrayList<>();
+	private String nom;
+	private ArrayList<Produit> listeProduit = new ArrayList<>();
+
 	public Client() {
-		
+
 	}
+
 	public Client(String nom) {
 		this.nom = nom;
-		
+
 	}
-	public void ajouterProduits(ArrayList<Produit> listeProduit){
-		
+
+	public void ajouterProduits(ArrayList<Produit> listeProduit) {
+
 		this.listeProduit = listeProduit;
 	}
-	
-	
+
+	public void calculerPrix() {
+		
+		double total;
+		
+		Produit[] produits = new Produit[listeProduit.size()];
+		
+		produits = listeProduit.toArray(produits);
+		
+		for(int i = 0; i < produits.length(); i++) {
+			
+			total += produits[i].getPrix()*produits[i].getQte();
+		}
+		System.out.println(this.nom+" "+total+"$ ");
+	}
 
 }

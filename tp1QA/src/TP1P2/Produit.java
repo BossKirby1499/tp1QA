ca4e1c7abcb935e3957fc80d792cb723d884668b
Unknown <karl.1993.aoun@gmail.com>
2019-02-06 11:54:10 -0500

    classe produit fin

--- tp1QA/src/TP1P2/Produit.java
+++ tp1QA/src/TP1P2/Produit.java
@@ -1,5 +1,41 @@
 package TP1P2;
 
 public class Produit {
+	private String nom;
+	private int prix;
+	private double qte;
 
+	public Produit() {
+
+	}
+
+	public Produit(String nom, int prix) {
+		this.nom = nom;
+		this.prix = prix;
+		this.qte = 0;
+	}
+
+	public String getNom() {
+		return this.nom;
+	}
+
+	public int getPrix() {
+		return this.getPrix();
+	}
+
+	public double getQte() {
+		return this.getQte();
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public void setQte(double qte) {
+		this.qte = qte;
+	}
 }
